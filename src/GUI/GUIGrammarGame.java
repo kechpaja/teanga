bec2fe749dafa45824ad167b0725e3a0cd7428f7
Danielle Rothermel <danielle_rothermel@brown.edu>
2012-04-22 17:15:52 -0400

    fixing Grammar Game

--- src/GUI/GUIGrammarGame.java
+++ src/GUI/GUIGrammarGame.java
@@ -11,6 +11,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Stack;
 
 import javax.imageio.ImageIO;
@@ -122,7 +123,7 @@ public class GUIGrammarGame extends JPanel{
 		int numChars = partialSentance.length();
 		Box vertBox = Box.createVerticalBox();
 		String actSent[] = partialSentance.split("~");//Need to fix this to be ~1~
-		Stack<String> words = new Stack<String>();
+		LinkedList<String> words = new LinkedList<String>();
 		
 		//parse the sentence into words with null's representing blanks
 		for(int i = 0; i < actSent.length; i++){
@@ -140,19 +141,73 @@ public class GUIGrammarGame extends JPanel{
 		
 		ArrayList<JLabel> spaces = new ArrayList<JLabel>();
 		
-		if(numChars > _maxChars*2){
+		if(numChars > _maxChars*2){//too long
 			System.out.println("Error, grammar game sentance too long");
 			//go to next exercise
-		} else if(numChars > _maxChars){
+		} else if(numChars > _maxChars){//will need 2 lines
+			ArrayList<JLabel> line1 = new ArrayList<JLabel>();
+			ArrayList<JLabel> line2 = new ArrayList<JLabel>();
+			int currNum = 0;
+			String currString = "";
 			
+			//fill in line1
+			while(currNum < _maxChars && !words.isEmpty()){
+				String last;
+				if((last = words.getLast()) == null){//when you get to a blank
+					JLabel phrase = new JLabel(currString);//put the beginning phrase in line
+					currString = "";
+					line1.add(phrase);
+					JLabel blank = new JLabel("          ");//put a blank in the line
+					line1.add(blank);
+					spaces.add(blank);//add blank label to list of blanks
+					currNum += 10;
+				} else{//otherwise, add word to line 1
+					currString += last;
+					currNum += last.length();
+				}
+			}
 			
+			//when you have finished line 1
+			while(!words.isEmpty()){
+				String last;
+				if((last = words.getLast()) == null){
+					JLabel phrase = new JLabel(currString);//put the beginning phrase in line
+					currString = "";
+					line2.add(phrase);
+					JLabel blank = new JLabel("          ");//put a blank in the line
+					line2.add(blank);
+					spaces.add(blank);//add blank label to list of blanks
+				} else{
+					currString += last;;
+				}
+			}
 			
+			//make the last section into a label in line2
+			if(!currString.equals("")){
+				JLabel phrase = new JLabel(currString);
+				line2.add(phrase);
+			}
 			
+			//TODO:make the actual box
+		} else{//Only 1 line
+			ArrayList<JLabel> line1 = new ArrayList<JLabel>();
+			String currString = "";
 			
-			
-			
-		} else{
-			
+			//Just have 1 line to add to
+			while(!words.isEmpty()){
+				String last;
+				if((last = words.getLast()) == null){
+					JLabel phrase = new JLabel(currString);//put the beginning phrase in line
+					currString = "";
+					line1.add(phrase);
+					JLabel blank = new JLabel("          ");//put a blank in the line
+					line1.add(blank);
+					spaces.add(blank);//add blank label to list of blanks
+				} else{
+					currString += last;;
+				}
+			}
+			//make the actual box	
 		}
 			/*
 			JLabel string1Label = new JLabel(string1);
