f43ceb5922d7594cfbdd1e178db7760fd45d2395
Tara Noble <tara_noble@brown.edu>
2012-04-22 16:10:14 -0400

    Please work

--- src/ELearning/Driver.java
+++ src/ELearning/Driver.java
@@ -1,9 +1,26 @@
 package ELearning;
 
+import java.io.*;
+import annie.*;
+
 public class Driver {
-	private VocabGameMaker vGameMaker;
-	private GrammarGameMaker gGameMaker;
-	private Exercises e;
+	VocabGameMaker vGameMaker;
+	GrammarGameMaker gGameMaker;
+	Exercises exercises;
+	HelpBox helpbox;
+	OpeningPage openingpage;
+	
+	public Driver(){
+		try{
+			exercises = new Exercises("Insert vfile here", "Insert gfile here");
+			helpbox = new HelpBox("insert v file here", "insert gfile here");
+			vGameMaker = new VocabGameMaker(exercises, helpbox);
+			gGameMaker = new GrammarGameMaker(exercises, helpbox);
+			openingpage = new OpeningPage();
+		} catch (IOException e){
+			System.out.println("Figure out what to do in this situation");
+		}
+	}
 	
 
 }
