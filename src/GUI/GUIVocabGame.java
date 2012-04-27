f4759b7935861240d16ecc5aa8cd37da16c1974b
Tara Noble <tara_noble@brown.edu>
2012-04-26 23:47:08 -0400

    dictionary now pauses the game

--- src/GUI/GUIVocabGame.java
+++ src/GUI/GUIVocabGame.java
@@ -6,6 +6,8 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.Box;
 import javax.swing.JButton;
@@ -118,14 +120,30 @@ public class GUIVocabGame extends JPanel{
 		}
 
 	}
-	
+
 	private class DictionaryButtonListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			_vocabLevel.decrementScore(2);
-			_gameBoard.pause();
 			DictionaryInternalFrame dictFrame = new DictionaryInternalFrame(_driver.getDictionary());
+			dictFrame.addWindowListener(new WindowListener() {
+	            public void windowClosed(WindowEvent arg0) {
+	            	System.out.println("here");
+	                _gameBoard.restart();
+	            }
+	            public void windowActivated(WindowEvent arg0) {
+	                _gameBoard.pause();
+	            }
+	            public void windowClosing(WindowEvent arg0) {}
+	            public void windowDeactivated(WindowEvent arg0) {
+	            	System.out.println("here3");
+	                _gameBoard.restart();
+	            }
+	            public void windowDeiconified(WindowEvent arg0) {}
+	            public void windowIconified(WindowEvent arg0) {}
+	            public void windowOpened(WindowEvent arg0) {}
+	        });
 
 			
 		}
