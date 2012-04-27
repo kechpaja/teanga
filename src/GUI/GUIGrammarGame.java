b79207a959256ba5a498103c8445e15b142c76b8
Tara Noble <tara_noble@brown.edu>
2012-04-27 01:31:17 -0400

    push it. push it real good.

--- src/GUI/GUIGrammarGame.java
+++ src/GUI/GUIGrammarGame.java
@@ -363,7 +363,9 @@ public class GUIGrammarGame extends JPanel{
 					_panel.revalidate();
 					
 				}
-			} else _choicePanel.notCorrect();
+			} else {
+				_driver.changePage(new GUIGrammarGame(_grammarLevel, _driver));
+			}
 			_score.setText(_grammarLevel.getScore() + "/" + _grammarLevel.getNecessaryScore()+"       ");
 		}
 	}
