6154d1d02d4459ed8ee8e1578a65e211043ccadb
Tara Noble <tara_noble@brown.edu>
2012-05-07 16:48:23 -0400

    added some grammar pics

--- src/GUI/GUIOptionsPage.java
+++ src/GUI/GUIOptionsPage.java
@@ -126,7 +126,7 @@ public class GUIOptionsPage extends JPanel{
         g3.dispose();
         ImageIcon newIcon3 = new ImageIcon(dst3);
 		
-		JButton back = new JButton("Redonu",newIcon3);
+		JButton back = new JButton("Reiru",newIcon3);
 		back.addActionListener(new BacktoBasicActionListener());
 		back.setSize(new Dimension(100, 30));
 		back.setLocation(875,0);
